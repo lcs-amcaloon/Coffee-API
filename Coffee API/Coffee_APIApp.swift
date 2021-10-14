4185945346fc7d815dba1d07e7478252e0165b6c
Angus McAloon <AMcAloon@lcs.on.ca>
2021-10-14 08:44:36 -0400

    Initial Commit

---
+++ Coffee API/Coffee_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_APIApp.swift
+//  Coffee API
+//
+//  Created by Angus McAloon on 2021-10-14.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
