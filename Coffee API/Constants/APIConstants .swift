ba50e5377902085b8439970007aca2e3edddda58
Angus McAloon <AMcAloon@lcs.on.ca>
2021-10-14 09:56:19 -0400

    Added the ability to get information from the isn file. and have it stored. added a MVVM structure for the code. Working on the UI

---
+++ Coffee API/Constants/APIConstants .swift
@@ -0,0 +1,16 @@
+//
+//  APIConstants .swift
+//  Coffee API
+//
+//  Created by Angus McAloon on 2021-10-14.
+//
+
+import Foundation
+
+//struct Constants {
+//    static let baseURl = ""
+//}
+
+enum APIConstants {
+    static let baseUrl = "https://coffee.alexflipnote.dev/random.json"
+}
